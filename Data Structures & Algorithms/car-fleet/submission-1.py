cb5585707944411260d8fff78df975c694bfd855
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-05-23 14:46:28 +0900

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_sp = [] 
+        for i in range(len(position)): 
+            pos_sp.append((position[i], speed[i])) 
+        pos_sp = sorted(pos_sp, key=lambda x: x[0]) 
+
+
+        stack = [] 
+        for pos, sp in pos_sp: 
+            miles = target - pos 
+            time = miles / sp 
+            while stack and stack[-1] <= time: 
+                stack.pop() 
+            stack.append(time) 
+        
+        return len(stack) 
+        
+    #     stack = [] 
+
+        
+    #     res = [0] * target 
+    #     for i in range(len(position)): 
+    #         pos = position[i] 
+    #         sp = speed[i] 
+    #         miles = target - pos 
+    #         time = miles / sp 
+    #         res[pos] = time
+        
+    #     cnt = 0
+    #     mx = -float("inf")
+    #     for i in range(target - 1, -1, -1): 
+    #         if res[i] != 0 and res[i] > mx: 
+    #             cnt += 1 
+    #             mx = res[i] 
+    #     return cnt 
+    
+    # # T: O(n) 
+    # # S: O(n) 
