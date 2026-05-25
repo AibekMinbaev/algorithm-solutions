dc6a448aff28adcd6fb4ec2dc9a33aa91924ba58
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-05-25 16:35:54 +0900

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool: 
+        if not root: 
+            return True 
+
+        # Iterative DFS 
+        mp = {None: 0}
+
+        stack = [root] 
+        while stack: 
+            node = stack[-1] 
+
+            if node.left and node.left not in mp: 
+                stack.append(node.left) 
+            elif node.right and node.right not in mp: 
+                stack.append(node.right) 
+            else: 
+                node = stack.pop()
+                if abs(mp[node.left] - mp[node.right]) > 1: 
+                    return False 
+                
+                mp[node] = 1 + max(mp[node.left], mp[node.right])
+
+        return True 
+                
+        # # Recursive DFS 
+        # # time: O(n) 
+        # # space: O(h) h is the height of the tree, recursion stack 
+        # res = True 
+        # def dfs(root): 
+        #     nonlocal res; 
+
+        #     if not root: 
+        #         return 0 
+            
+        #     left = dfs(root.left) 
+        #     right = dfs(root.right) 
+
+        #     res = res and abs(left - right) <= 1
+        #     return 1 + max(left, right)
+        
+        # dfs(root)
+        # return res 
