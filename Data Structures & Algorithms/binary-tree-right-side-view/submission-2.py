65f52fae0e930992f295d2743beade32381c5c5b
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-05-26 21:52:31 +0900

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]: 
+        # Iterative BFS 
+        # time: O(n) 
+        # space: O(n) 
+
+        if not root: 
+            return [] 
+        
+        mp = defaultdict(int) 
+        stack = [(root, 0)] 
+
+        while stack: 
+            node, level = stack.pop() 
+            mp[level] = node.val 
+
+            if node.right: 
+                stack.append((node.right, level+1)) 
+            
+            if node.left: 
+                stack.append((node.left, level+1)) 
+            
+        res = sorted(mp.items(), key=lambda k: k[0]) 
+        res = [val for key, val in res] 
+        return res 
+
+
+        # BFS solution 
+        # time:  O(n) 
+        # space: O(n) 
+        if not root: 
+            return [] 
+
+        q = deque([root]) 
+
+        res = [] 
+
+        while q: 
+            right_val = None 
+            for _ in range(len(q)): 
+                node = q.popleft()
+                right_val = node.val 
+
+                if node.left: 
+                    q.append(node.left) 
+                if node.right: 
+                    q.append(node.right) 
+            
+            res.append(right_val) 
+        return res 
+
