86ab609e2b01a1ad8bb96ed7883574d5c0951215
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-05-30 16:09:06 +0900

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool: 
+        # DFS solution 
+        # Detecting the cycle in the prerequitise is the key
+        
+        mp = defaultdict(list) 
+        for a, b in prerequisites: 
+            mp[a].append(b) 
+
+        def dfs(course: int, seen: Set[int]): 
+            if course in seen: 
+                return False 
+            
+            if course not in mp: 
+                return True
+
+            seen.add(course) 
+            for p in mp[course]: 
+                if not dfs(p, seen.copy()): 
+                    return False 
+            
+            del mp[course]
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course, set()): 
+                return False 
+        return True 
+
+# [0,1], [1, 2], [2, 3] 
+
+# # 0 > 1 > 2 > 3 
