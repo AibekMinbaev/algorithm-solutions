103dc5c034b5d77b2908b31e7846116bd0324eb4
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-06-02 07:13:26 +0900

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int: 
+        # DP: memoization 
+        # time: n 
+        # space: n 
+
+
+        memo = [-1] * len(cost) 
+
+        def helper(step: int) -> int: 
+            if step >= len(cost): 
+                return 0 
+            
+            if memo[step] != -1: 
+                return memo[step]
+                
+            memo[step] = cost[step] + min(helper(step + 1), helper(step + 2)) 
+            return memo[step]
+
+        return min(helper(0), helper(1))
+
+
+        # Brute force 
+        # time: 2^n 
+        # space: n 
+
+        def helper(step: int) -> int: 
+            if step >= len(cost): 
+                return 0 
+            
+            return cost[step] + min(helper(step + 1), helper(step + 2)) 
+
+        return min(helper(0), helper(1))
