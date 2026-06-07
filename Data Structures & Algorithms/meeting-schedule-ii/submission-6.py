63e47dafbd0ba0b53037740421ab0f4aef01d57e
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-06-07 13:14:01 +0900

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,41 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int: 
+        # Min Heap: 
+        # time: nlogn 
+        # space: n 
+        intervals = sorted(intervals, key=lambda x: x.start) 
+
+        rooms = []
+        res = 0 
+        for interval in intervals: 
+            if rooms and rooms[0] <= interval.start: 
+                heapq.heappop(rooms) 
+            heapq.heappush(rooms, interval.end) 
+            res = max(res, len(rooms))
+        return res
+
+        # # Brute force: 
+        # # time: n^2 
+        # # space: n 
+        # intervals = sorted(intervals, key=lambda x: x.start) 
+
+        # ends = []
+        # for interval in intervals: 
+        #     for i in range(len(ends)): 
+        #         if ends[i] <= interval.start: 
+        #             ends[i] = interval.end 
+        #             break 
+        #     else: 
+        #         ends.append(interval.end) 
+        # return len(ends) 
+            
+
+
