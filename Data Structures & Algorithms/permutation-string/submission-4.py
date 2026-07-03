e25c5940c3f6ddb117ae0f957017193d9eef0c2a
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2026-07-03 20:56:18 +0900

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,64 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s2) < len(s1): 
+            return False 
+
+        s1_freq = [0] * 26 
+        for ch in s1: 
+            idx = ord(ch) - ord('a') 
+            s1_freq[idx] += 1 
+        
+        s2_freq = [0] * 26
+    
+        l, r = 0, 0
+        while r < len(s2): 
+
+            while r - l + 1 <= len(s1): 
+                idx = ord(s2[r]) - ord('a') 
+                s2_freq[idx] += 1 
+                r += 1 
+
+            if s1_freq == s2_freq: 
+                return True 
+            
+            idx = ord(s2[l]) - ord('a')
+            s2_freq[idx] -= 1 
+            l += 1 
+
+        return False 
+
+
+
+
+
+
+
+        # if len(s2) < len(s1): 
+        #     return False 
+
+        # freq = {} 
+        # for ch in s1: 
+        #     if ch not in freq: 
+        #         freq[ch] = 0 
+        #     freq[ch] += 1
+
+        # l, r = 0, 0
+        # while r < len(s2):
+
+        #     if s2[r] not in freq: 
+        #         while l <= r: 
+        #             if s2[l] in freq: 
+        #                 freq[s2[l]] += 1 
+        #             l += 1 
+        #     else: 
+        #         while not freq[s2[r]]: 
+        #             freq[s2[l]] += 1
+        #             l += 1 
+        #         freq[s2[r]] -= 1 
+        #         if r - l + 1 == len(s1): 
+        #             return True 
+        #     r += 1 
+        # return False 
+
+            
